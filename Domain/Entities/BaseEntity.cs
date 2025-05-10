3dcdf0b0543bdbf5f3bb4c2d9310fe528a5d5622
Ahmedabnaser <ahmeducfjf@gmail.com>
2025-05-11 02:25:38 +0300

    Adding All The Models and their Configurations

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public DateTime? DeletedOnUtc { get; private set; }
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            DeletedOnUtc = DateTime.UtcNow;
+        }
+
+        public void Restore()
+        {
+            IsDeleted = false;
+            DeletedOnUtc = null;
+        }
+    }
+}
---
+++ Persistence/Context/Confiugration/BillingConfigurations.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Domain.Enums; 
+
+namespace Persistence.Context.Confiugration
+{
+    public class BillingConfigurations:IEntityTypeConfiguration<Billing>
+    {
+        public void Configure(EntityTypeBuilder<Billing> builder)
+        {
+            builder.Property(b => b.ServicesRendered)
+          .HasConversion(
+              v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+              v => JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions)null)
+          );
+
+            builder.Property(b => b.TotalCost)
+                .HasPrecision(18,2);
+
+            builder.Property(b => b.Status)
+                .HasConversion<string>(); // Store enum as string
+
+            // Relationships
+            builder.HasMany(b => b.Payments)
+                .WithOne(p => p.Billing)
+                .HasForeignKey(p => p.BillingId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }   
+    }
+}
