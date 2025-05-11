75f6801084a1a4aa6cfa35fed1b5b800bbbe8edd
Ahmedabnaser <ahmeducfjf@gmail.com>
2025-05-11 16:32:36 +0300

    Adding JWt implementation

--- TalkSpace.Api/Extentions/ServiceCollectionExtensions.cs
+++ TalkSpace.Api/Extentions/ServiceCollectionExtensions.cs
@@ -1,12 +1,17 @@
-﻿using Domain.Data;
+﻿using Application.Abstractions;
+using Application.Services;
+using Domain.Data;
 using Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Persistence.Context;
 using Persistence.DbInitialization;
 using System;
+using System.Text;
 
 namespace TalkSpace.Api.Extensions
 {
@@ -17,7 +22,9 @@ namespace TalkSpace.Api.Extensions
             ConfigureAppData(services, configuration);
             AddDatabase(services, configuration);
             AddIdentityServices(services);
+            AddJwtAuthentication(services, configuration);
             AddMappingServices(services);
+            services.AddScoped<IJWtTokenService, JWtTokenService>();
 
             return services;
         }
@@ -58,5 +65,29 @@ namespace TalkSpace.Api.Extensions
         {
             services.Configure<DatabaseConnections>(configuration.GetSection("ConnectionStrings"));
         }
+
+        private static void AddJwtAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
+                };
+            });
+        }
+
     }
 }
