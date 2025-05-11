a48de69e28ef5e2914bed63d15c4dbe995fe342d
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2025-05-11 23:31:04 +0300

    Implement Result Pattern.

---
+++ Domain/Results/QueryResult.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Results
+{
+    public class QueryResult<T> : Result<T>
+    {
+        public QueryResult(bool isFound, bool isSuccess, T? value) : base(isSuccess, value)
+        {
+            this.IsFound = isFound;
+        }
+
+        public QueryResult(bool isFound, bool isSuccess, string error, ErrorSource errorSource) : base(isSuccess, error, errorSource)
+        {
+            this.IsFound = isFound;
+        }
+        public bool IsFound { get; }
+        public bool IsNotFound => !this.IsFound;
+
+        public static new QueryResult<T> Success(T value) => 
+            new QueryResult<T>(true, true, value);
+
+        public static QueryResult<T> Failure(string error) =>
+            new QueryResult<T>(false, false, error, ErrorSource.Database);
+
+        public static QueryResult<T> NotFound() =>
+            new QueryResult<T>(false, true, default);
+    }
+}
