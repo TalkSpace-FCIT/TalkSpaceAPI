e748fee5de2ba68415e3a5f2a99f61664e63574f
Ahmedabnaser <ahmeducfjf@gmail.com>
2025-05-13 03:04:13 +0300

    Delete redudndat implementation of Grenerirc repo

--- Persistence/Repositories/AppointmentRepository.cs
+++ Persistence/Repositories/AppointmentRepository.cs
@@ -13,66 +13,17 @@ using System.Threading.Tasks;
 
 namespace Persistence.Repositories
 {
-    public class AppointmentRepository : IAppointmentRepository
+    public class AppointmentRepository : Repository<Appointment>,IAppointmentRepository
     {
         private readonly AppDbContext context;
         private DbSet<Appointment> _dbset;
 
-        public AppointmentRepository(AppDbContext context)
+        public AppointmentRepository(AppDbContext context):base(context)
         {
             this.context = context;
             _dbset = context.Set<Appointment>();
         }
-        public async Task AddAsync(Appointment entity)
-        {
-          await _dbset.AddAsync(entity);
-           await context.SaveChangesAsync();
-        }
-
-        public async Task AddRangeAsync(IEnumerable<Appointment> entities)
-        {
-            await _dbset.AddRangeAsync(entities);
-            await context.SaveChangesAsync();
-        }
-
-        public Task<int> CountAsync(Expression<Func<Appointment, bool>>? predicate = null)
-        {
-            return predicate == null
-                ? _dbset.CountAsync()
-                : _dbset.CountAsync(predicate);
-        }
-
-        public async Task<bool> ExistsAsync(Expression<Func<Appointment, bool>> predicate)
-        {
-            return await _dbset.AnyAsync(predicate);
-        }
-
-
-        public async Task<IEnumerable<Appointment>> FindAsync(
-            Expression<Func<Appointment, bool>> predicate,
-            Func<IQueryable<Appointment>, IIncludableQueryable<Appointment, object>>? include = null)
-        {
-            IQueryable<Appointment> query = _dbset.Where(predicate);
-
-            if (include != null)
-            {
-                query = include(query);
-            }
-
-            return await query.ToListAsync();
-        }
-        public async Task<IEnumerable<Appointment>> GetAllAsync(Func<IQueryable<Appointment>, IIncludableQueryable<Appointment, object>>? include = null)
-        {
-            IQueryable<Appointment> query = _dbset;
-
-            if (include != null)
-            {
-                query = include(query);
-            }
-
-            return await query.ToListAsync();
-        }
-
+       
         public async Task<IEnumerable<Appointment>> GetAppointmentsByDoctorIdAsync(string DoctorID)
         {
             return await _dbset.Where(a=>a.DoctorId==DoctorID)
@@ -90,34 +41,12 @@ namespace Persistence.Repositories
                 .ToListAsync();
         }
 
-        public async Task<Appointment?> GetByIdAsync<TId>(TId id) where TId : notnull
-        {
-            return await _dbset.FindAsync(id);
-        }
-
-        public void Remove(Appointment entity)
-        {
-            _dbset.Remove(entity);
-            context.SaveChanges();
-        }
-
-        public void RemoveRange(IEnumerable<Appointment> entities)
-        {
-            _dbset.RemoveRange(entities);
-            context.SaveChanges();  
-        }
-
         public async Task ScheduleAppointmentAsync(Appointment appointment)
         {
              await _dbset.AddAsync(appointment);
-            await context.SaveChangesAsync();
+           
         }
 
-        public void Update(Appointment entity)
-        {
-           _dbset.Update(entity);
-            context.SaveChanges();
-        }
 
 
     }
